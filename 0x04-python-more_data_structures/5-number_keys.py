560b509117c1d9903c00194fb16ea329781e79fb
daniloseibonsu <daniloseibonsu@gmail.com>
2023-12-10 08:07:42 -0800

    5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    "returns the number of keys in a dictionary"
 
-    return(len(a_dictionary))
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
