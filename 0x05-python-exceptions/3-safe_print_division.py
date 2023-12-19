1506d3a572ccd14a842d6447de313dd0a132fcb5
daniloseibonsu <daniloseibonsu@gmail.com>
2023-12-19 10:42:48 -0800

    123

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     "divides 2 integers and prints the result"
     try:
         result = a / b
-    except:
+    except (TypeError, ZeroDivisionError):
         result = None
     finally:
         print("Inside result: {}".format(result))
