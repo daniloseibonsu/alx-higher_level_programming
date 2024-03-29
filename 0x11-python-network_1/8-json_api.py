8534753d63130873dd7d5176cc68ade33da9ba15
daniloseibonsu <daniloseibonsu@gmail.com>
2024-03-29 11:51:37 -0700

    fixes

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""sends a POST request to http://0.0.0.0:5000/search_user
-   with a passed letter as a parameter"""
-from sys import argv
-from requests import post
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
 
 
-if __name__ == '__main__':
-    q = ""
-    if len(argv) == 2:
-        q = argv[1]
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        request = post('http://0.0.0.0:5000/search_user', data={'q': q})
-        r_dict = request.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print("Not a valid JSON")
